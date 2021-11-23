7a5b8d1b374a20c731794d7ef0d820a738eaeca1
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-23 12:16:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 24
+let num4 = 6
+let mod = num3/num4
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
